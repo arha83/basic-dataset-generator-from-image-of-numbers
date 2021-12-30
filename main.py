f655024331ad58a7aa5a463386a71a317dc4c8b0
arha83 <arha113918@gmail.com>
2021-12-30 22:36:26 +0330

    getting bounding rectangles and printing index

--- main.py
+++ main.py
@@ -17,7 +17,17 @@ for index, contour in enumerate(contours_all):
     if hierarchy[3] != -1 and (hierarchy[0] != -1 or hierarchy[1] != -1):
         contours.append(contour)
 
+rectangles_shuffled= []
+for contour in contours:
+    x, y, w, h= cv.boundingRect(contour)
+    rectangles_shuffled.append((x, y, w, h))
+    cv.rectangle(baseImage_color, (x,y), (x+w,y+h), (255,0,0), 1)
 
+for index, rectangle in enumerate(rectangles):
+    cv.putText(baseImage_color, str(index), (rectangle[0],rectangle[1]), cv.FONT_HERSHEY_SIMPLEX, 0.25, (255,0,0))
+
+print(rectangles, len(rectangles))
 cv.drawContours(baseImage_color, contours_all, -1, (0,255,0), 1)
+cv.drawContours(baseImage_color, contours, -1, (0,0,255), 1)
 cv.imshow('images', baseImage_color)
 cv.waitKey(0)
